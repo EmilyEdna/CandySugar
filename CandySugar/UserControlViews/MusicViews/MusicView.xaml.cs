4cc5ce1c70034157fe9dff6ba7195a28f3deb327
EmilyEdna <847432003@qq.com>
2022-02-09 18:05:38 +0800

    重构第十天V3.0

--- CandySugar/UserControlViews/MusicViews/MusicView.xaml.cs
+++ CandySugar/UserControlViews/MusicViews/MusicView.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -24,5 +25,31 @@ namespace CandySugar.UserControlViews.MusicViews
         {
             InitializeComponent();
         }
+        #region 音量
+        /// <summary>
+        /// 音量设置
+        /// </summary>
+        private int VolumeAnime = 0;
+        private void VolumeClick(object sender, MouseButtonEventArgs e)
+        {
+            if (VolumeAnime == 0)
+            {
+                BeginStoryboard((Storyboard)FindResource("Open"));
+                VolumeAnime = 1;
+            }
+            else
+            {
+                BeginStoryboard((Storyboard)FindResource("Close"));
+                VolumeAnime = 0;
+            }
+        }
+
+        private void VolumeChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = (sender as Slider);
+            VolumeShow.Content = (int)slider.Value + "%";
+            //MediaPlay.Volume = slider.Value / 100;
+        }
+        #endregion
     }
 }
