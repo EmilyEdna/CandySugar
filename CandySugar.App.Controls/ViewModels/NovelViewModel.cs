8900c29cfb142a81a0b03c6d3ea7988615d87275
EmilyEdna <847432003@qq.com>
2022-03-04 17:43:39 +0800

    xam v1.7

--- CandySugar.App.Controls/ViewModels/NovelViewModel.cs
+++ CandySugar.App.Controls/ViewModels/NovelViewModel.cs
@@ -1,14 +1,22 @@
 ﻿using CandySugar.Xam.Common;
+using Novel.SDK;
+using Novel.SDK.ViewModel;
+using Novel.SDK.ViewModel.Enums;
 using Novel.SDK.ViewModel.Request;
+using Novel.SDK.ViewModel.Response;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using XF.Material.Forms.UI.Dialogs;
+using XF.Material.Forms.UI.Dialogs.Configurations;
 
 namespace CandySugar.App.Controls.ViewModels
 {
-    public class NovelViewModel: ViewModelBase
+    public class NovelViewModel : ViewModelBase
     {
-        private readonly NovelProxy Proxy;
+        private readonly  NovelProxy Proxy;
         public NovelViewModel()
         {
             Proxy = new NovelProxy
@@ -18,6 +26,41 @@ namespace CandySugar.App.Controls.ViewModels
                 PassWord = Soft.ProxyPwd,
                 UserName = Soft.ProxyAccount
             };
+            Init();
+        }
+
+        private ObservableCollection<NovelCategoryResult> _NovelCategory;
+        public ObservableCollection<NovelCategoryResult> NovelCategory
+        {
+            get { return _NovelCategory; }
+            set { SetProperty(ref _NovelCategory, value); }
+        }
+
+
+        private async void Init()
+        {
+            try
+            {
+                var NovelInit = await NovelFactory.Novel(opt =>
+                {
+                    opt.RequestParam = new NovelRequestInput
+                    {
+                        CacheSpan = Soft.CacheTime,
+                        NovelType = NovelEnum.Init,
+                        Proxy = this.Proxy
+                    };
+                }).RunsAsync();
+                this.NovelCategory = new ObservableCollection<NovelCategoryResult>(NovelInit.IndexCategories);
+                //this.NovelRecommend = new ObservableCollection<NovelRecommendResult>(NovelInit.IndexRecommends);
+                throw new Exception();
+            }
+            catch(Exception ex)
+            {
+                using (await MaterialDialog.Instance.LoadingSnackbarAsync("网络有波动，请稍后再试~`(*>﹏<*)′"))
+                {
+                    await Task.Delay(3000);
+                }
+            }
         }
     }
 }
--- CandySugar.Xam.Common/Soft.cs
+++ CandySugar.Xam.Common/Soft.cs
@@ -6,9 +6,10 @@ namespace CandySugar.Xam.Common
 {
     public class Soft
     {
-        public static string ProxyIP { get; set; }
-        public static int ProxyPort { get; set; }
+        public static string ProxyIP { get; set; } = "";
+        public static int ProxyPort { get; set; } = -1;
         public static string ProxyPwd { get; set; }
         public static string ProxyAccount { get; set; }
+        public static int CacheTime { get; set; } = 60;
     }
 }
