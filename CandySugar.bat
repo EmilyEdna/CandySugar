8833412cda522579a2005dd05b52729385319e97
EmilyEdna <847432003@qq.com>
2022-03-21 14:48:36 +0800

    bat

--- CandySugar.bat
+++ CandySugar.bat
@@ -1,6 +1,6 @@
 cd /d %~dp0
-dotnet publish CandySugar\CandySugar.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows --sc true -r win-x64
-dotnet publish CandySugar.Upgrade\CandySugar.Upgrade.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows --sc true -r win-x64
+dotnet publish CandySugar\CandySugar.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows10.0.17763.0 --sc true -r win-x64
+dotnet publish CandySugar.Upgrade\CandySugar.Upgrade.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows10.0.17763.0 --sc true -r win-x64
 cd PublishSoft
 del *.pdb
 cd ..
@@ -10,5 +10,5 @@ rd /S /Q CandySugar.Common\obj CandySugar.Common\bin\Release
 rd /S /Q CandySugar.Controls\obj CandySugar.Controls\bin\Release
 rd /S /Q CandySugar.Resource\obj CandySugar.Resource\bin\Release
 rd /S /Q CandySugar.Upgrade\obj CandySugar.Upgrade\bin\Release
-xcopy CandySugar\bin\Debug\net6.0-windows\Plugins PublishSoft\Plugins /e /s
+xcopy CandySugar\bin\Debug\net6.0-windows10.0.17763.0\Plugins PublishSoft\Plugins /e /s
 pause
