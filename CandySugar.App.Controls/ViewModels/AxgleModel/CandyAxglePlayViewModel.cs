399afcd0a3ee575a975712472cf58401ea685ac6
EmilyEdna <847432003@qq.com>
2022-03-30 17:31:34 +0800

    xam v8.1

--- CandySugar.App.Controls/ViewModels/AxgleModel/CandyAxglePlayViewModel.cs
+++ CandySugar.App.Controls/ViewModels/AxgleModel/CandyAxglePlayViewModel.cs
@@ -10,5 +10,21 @@ namespace CandySugar.App.Controls.ViewModels.AxgleModel
         public CandyAxglePlayViewModel(INavigationService navigationService) : base(navigationService)
         {
         }
+
+        #region Property
+        private string _PlayURL;
+        public string PlayURL
+        {
+            get { return _PlayURL; }
+            set { SetProperty(ref _PlayURL, value); }
+        }
+        #endregion
+
+        #region Override
+        public override void Initialize(INavigationParameters parameters)
+        {
+            PlayURL = parameters.GetValue<string>("Route");
+        }
+        #endregion
     }
 }
--- CandySugar.App.Controls/Views/Axgle/CandyAxglePlayView.xaml.cs
+++ CandySugar.App.Controls/Views/Axgle/CandyAxglePlayView.xaml.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using CandySugar.Xam.Common.Platform;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Prism.Ioc;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Plugin.DeviceOrientation;
+using Plugin.DeviceOrientation.Abstractions;
+using CandySugar.App.Controls.ViewModels.AxgleModel;
+using CandySugar.Xam.Common;
 
 namespace CandySugar.App.Controls.Views.Axgle
 {
@@ -15,6 +20,25 @@ namespace CandySugar.App.Controls.Views.Axgle
         public CandyAxglePlayView()
         {
             InitializeComponent();
+        }
+
+        private async void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            ContainerLocator.Container.Resolve<IAndroidPlatform>().HiddenStatusBar();
+
+            CrossDeviceOrientation.Current.LockOrientation(DeviceOrientations.Landscape);
+            var PlayURL = (this.BindingContext as CandyAxglePlayViewModel).PlayURL;
+            HtmlWebViewSource Source = new HtmlWebViewSource();
+            Source.Html = Extension.ReadLocalHtml("axgle");
+            web.Source = Source;
+            await Task.Delay(3000);
+            await web.EvaluateJavaScriptAsync($"Init('{PlayURL}')");
+        }
+
+        private void ContentPage_Disappearing(object sender, EventArgs e)
+        {
+            ContainerLocator.Container.Resolve<IAndroidPlatform>().ShowStatusBar();
+            CrossDeviceOrientation.Current.LockOrientation(DeviceOrientations.Portrait);
         }
     }
 }
