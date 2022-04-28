3c7bdb1d84bbe287ef16e5af532f35e42e16f525
EmilyEdna <847432003@qq.com>
2022-04-28 17:17:33 +0800

    修改脚本

--- CandySugar.bat
+++ CandySugar.bat
@@ -1,10 +1,10 @@
 cd /d %~dp0
-dotnet publish CandySugar\CandySugar.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows10.0.17763.0 --sc true -r win-x64
+dotnet publish CandySugar.WPF\CandySugar.WPF.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows10.0.17763.0 --sc true -r win-x64
 dotnet publish CandySugar.Upgrade\CandySugar.Upgrade.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows10.0.17763.0 --sc true -r win-x64
 cd PublishSoft
 del *.pdb
 cd ..
-rd /S /Q CandySugar\obj CandySugar\bin\Release
+rd /S /Q CandySugar.WPF\obj CandySugar.WPF\bin\Release
 rd /S /Q CandySugar.Core\obj CandySugar.Core\bin\Release
 rd /S /Q CandySugar.Common\obj CandySugar.Common\bin\Release
 rd /S /Q CandySugar.Controls\obj CandySugar.Controls\bin\Release
