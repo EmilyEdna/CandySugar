04407fa4ef2f11eb9e088ad3889dc019879129bb
EmilyEdna <847432003@qq.com>
2022-05-06 11:50:43 +0800

    更换项目结构

--- CandySugar.bat
+++ CandySugar.bat
@@ -1,16 +1,16 @@
 chcp 65001
 @echo 开始自动化发布
 cd /d %~dp0
-dotnet publish CandySugar.WPF\CandySugar.WPF.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows10.0.17763.0 --sc true -r win-x64
-dotnet publish CandySugar.Upgrade\CandySugar.Upgrade.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows10.0.17763.0 --sc true -r win-x64
+dotnet publish 桌面端\CandySugar.WPF\CandySugar.WPF.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows10.0.17763.0 --sc true -r win-x64
+dotnet publish 桌面端\CandySugar.Upgrade\CandySugar.Upgrade.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows10.0.17763.0 --sc true -r win-x64
 
-rd /S /Q CandySugar.WPF\obj CandySugar.WPF\bin\Release
-rd /S /Q CandySugar.Core\obj CandySugar.Core\bin\Release
-rd /S /Q CandySugar.Common\obj CandySugar.Common\bin\Release
-rd /S /Q CandySugar.Controls\obj CandySugar.Controls\bin\Release
-rd /S /Q CandySugar.Resource\obj CandySugar.Resource\bin\Release
-rd /S /Q CandySugar.Upgrade\obj CandySugar.Upgrade\bin\Release
-xcopy CandySugar.WPF\bin\Debug\net6.0-windows10.0.17763.0\Plugins PublishSoft\Plugins /e /s
+rd /S /Q 桌面端\CandySugar.WPF\obj 桌面端\CandySugar.WPF\bin\Release
+rd /S /Q 桌面端\CandySugar.Core\obj 桌面端\CandySugar.Core\bin\Release
+rd /S /Q 桌面端\CandySugar.Common\obj 桌面端\CandySugar.Common\bin\Release
+rd /S /Q 桌面端\CandySugar.Controls\obj 桌面端\CandySugar.Controls\bin\Release
+rd /S /Q 桌面端\CandySugar.Resource\obj 桌面端\CandySugar.Resource\bin\Release
+rd /S /Q 桌面端\CandySugar.Upgrade\obj 桌面端\CandySugar.Upgrade\bin\Release
+xcopy 桌面端\CandySugar.WPF\bin\Debug\net6.0-windows10.0.17763.0\Plugins PublishSoft\Plugins /e /s
 
 cd PublishSoft
 del *.pdb
