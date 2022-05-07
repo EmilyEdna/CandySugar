d8e59800bb1f83914d2b74290a77cbd866bfa9e8
EmilyEdna <847432003@qq.com>
2022-05-07 21:55:33 +0800

    zh->en dir fix can't find file bug with mobile platforms

--- CandySugar.bat
+++ CandySugar.bat
@@ -1,16 +1,16 @@
 chcp 65001
 @echo 开始自动化发布
 cd /d %~dp0
-dotnet publish 桌面端\CandySugar.WPF\CandySugar.WPF.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows10.0.17763.0 --sc true -r win-x64
-dotnet publish 桌面端\CandySugar.Upgrade\CandySugar.Upgrade.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows10.0.17763.0 --sc true -r win-x64
+dotnet publish DesktopPlatform\CandySugar.WPF\CandySugar.WPF.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows10.0.17763.0 --sc true -r win-x64
+dotnet publish DesktopPlatform\CandySugar.Upgrade\CandySugar.Upgrade.csproj -c Release -o ..\CandySugar\PublishSoft -f net6.0-windows10.0.17763.0 --sc true -r win-x64
 
-rd /S /Q 桌面端\CandySugar.WPF\obj 桌面端\CandySugar.WPF\bin\Release
-rd /S /Q 桌面端\CandySugar.Core\obj 桌面端\CandySugar.Core\bin\Release
-rd /S /Q 桌面端\CandySugar.Common\obj 桌面端\CandySugar.Common\bin\Release
-rd /S /Q 桌面端\CandySugar.Controls\obj 桌面端\CandySugar.Controls\bin\Release
-rd /S /Q 桌面端\CandySugar.Resource\obj 桌面端\CandySugar.Resource\bin\Release
-rd /S /Q 桌面端\CandySugar.Upgrade\obj 桌面端\CandySugar.Upgrade\bin\Release
-xcopy 桌面端\CandySugar.WPF\bin\Debug\net6.0-windows10.0.17763.0\Plugins PublishSoft\Plugins /e /s
+rd /S /Q DesktopPlatform\CandySugar.WPF\obj DesktopPlatform\CandySugar.WPF\bin\Release
+rd /S /Q DesktopPlatform\CandySugar.Core\obj DesktopPlatform\CandySugar.Core\bin\Release
+rd /S /Q DesktopPlatform\CandySugar.Common\obj DesktopPlatform\CandySugar.Common\bin\Release
+rd /S /Q DesktopPlatform\CandySugar.Controls\obj DesktopPlatform\CandySugar.Controls\bin\Release
+rd /S /Q DesktopPlatform\CandySugar.Resource\obj DesktopPlatform\CandySugar.Resource\bin\Release
+rd /S /Q DesktopPlatform\CandySugar.Upgrade\obj DesktopPlatform\CandySugar.Upgrade\bin\Release
+xcopy DesktopPlatform\CandySugar.WPF\bin\Debug\net6.0-windows10.0.17763.0\Plugins PublishSoft\Plugins /e /s
 
 cd PublishSoft
 del *.pdb
