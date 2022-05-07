c3215c6f4f22578aed8723bb27be8da349ecda46
EmilyEdna <847432003@qq.com>
2022-05-07 15:27:33 +0800

    MAUI Ver 1.1

---
+++ 混合端/CandySugar.Mix.Controls/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CandySugar.Mix.Controls
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ 混合端/CandySugar.Mix.Controls/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CandySugar.Mix.Controls
+{
+    // All the code in this file is only included on Tizen.
+    public class PlatformClass1
+    {
+    }
+}
