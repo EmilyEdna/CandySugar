dd4900b6f4bbcece435628f9ff5daceafd703466
EmilyEdna <847432003@qq.com>
2022-05-09 17:05:22 +0800

    全局异常

--- MixPlatform/CandySugar.MixUI/Platforms/Android/MainActivity.cs
+++ MixPlatform/CandySugar.MixUI/Platforms/Android/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Runtime;
+using Android.Widget;
 
 namespace CandySugar.MixUI;
 
@@ -13,4 +15,41 @@ namespace CandySugar.MixUI;
     ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        Platform.Init(this, savedInstanceState);
+        AndroidEnvironment.UnhandledExceptionRaiser += AndroidException;
+    }
+
+    public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+    {
+        Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    /// <summary>
+    /// 全局异常
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void AndroidException(object sender, RaiseThrowableEventArgs e)
+    {
+        var x = e.Exception;
+
+        //提示
+        Task.Run(() =>
+        {
+            Looper.Prepare();
+            //可以换成更友好的提示
+            Toast.MakeText(this, "很抱歉,程序出现异常,即将退出.", ToastLength.Long).Show();
+            Looper.Loop();
+        });
+
+        //停一会，让前面的操作做完
+        Thread.Sleep(2000);
+
+        e.Handled = true;
+    }
 }
