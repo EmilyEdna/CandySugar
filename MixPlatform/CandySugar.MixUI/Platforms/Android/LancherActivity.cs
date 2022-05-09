365f60410922292aa0d23073c4c3b29871d2c68d
EmilyEdna <847432003@qq.com>
2022-05-09 15:36:04 +0800

    MAUI Ver 1.3

---
+++ MixPlatform/CandySugar.MixUI/Platforms/Android/LancherActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content;
+
+namespace CandySugar.MixUI.Platforms.Android
+{
+    [Activity(Theme = "@style/CustomerStyle", MainLauncher = true, NoHistory = true)]
+    public class LancherActivity : MauiAppCompatActivity
+    {
+        protected async override void OnResume()
+        {
+            base.OnResume();
+            await Task.Delay(3000);
+            StartActivity(new Intent(Application.ApplicationContext, typeof(MainActivity)));
+        }
+    }
+}
